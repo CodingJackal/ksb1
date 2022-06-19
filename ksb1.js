4efd81f292ec578a472cdaec778617776af5ea11
CodingJackal <just@fakemail.com>
2022-06-19 23:53:50 +0200

    Zahlenformat angepasst und Prettier fuer die Formatierung aktiviert

--- ksb1.js
+++ ksb1.js
@@ -1,104 +1,109 @@
-'set strict'
+"set strict";
 
 /*
  * Ohne Funktion.
  */
-function clickBtn1() {    
+function clickBtn1() {
   //alert(rawData.length);
 }
 
 function processData(rawData) {
-	const rowArray = rawData.split("\n");	
-	header = createHeader(rowArray);
-	dataObjects = createData(rowArray, header, emptyFirstRows);
+  const rowArray = rawData.split("\n");
+  header = createHeader(rowArray);
+  dataObjects = createData(rowArray, header, emptyFirstRows);
 
-	showFileStats(rowArray);
-	//createHeadElements(header);	
-	//outputArea.value = findRowByName(header, "Gegen");
+  showFileStats(rowArray);
+  //createHeadElements(header);
+  //outputArea.value = findRowByName(header, "Gegen");
 }
 
 /*
- * Berechnet die Summe der Buchungen. Ohne 
+ * Berechnet die Summe der Buchungen. Ohne
  * gesetzten Filter werden alle Buchungen summiert.
  */
 function calculateSum(filterOne, filterTwo) {
-	const betragIndex = findRowByName(betragKopf);
-	let summe = 0;
-	for (let i = 0; i < dataObjects.length; i++) {
-		dataObjects[i].forEach((value, index) => {
-			if (index == betragIndex) {
-				summe += value;
-			}
-		});
-	}
-	return summe;
+  const betragIndex = findRowByName(betragKopf);
+  let summe = 0;
+  for (let i = 0; i < dataObjects.length; i++) {
+    dataObjects[i].forEach((value, index) => {
+      if (index == betragIndex) {
+        summe += value;
+      }
+    });
+  }
+  return summe;
 }
 
 function findRowByName(searchStr) {
-	let index = -1;
-	for (let i = 0; i < header.length; i++) {
-		if (header[i].includes(searchStr)) {
-			index = i;
-			break;
-		}
-	}
-	return index;
+  let index = -1;
+  for (let i = 0; i < header.length; i++) {
+    if (header[i].includes(searchStr)) {
+      index = i;
+      break;
+    }
+  }
+  return index;
 }
 
 function showFileStats(oneRow) {
-	const creationDate = oneRow[2].split("\t")[0];
-	const hardcodedSum = oneRow.at(-2).split("\t")[7];
+  const creationDate = oneRow[2].split("\t")[0];
+  const hardcodedSum = oneRow.at(-2).split("\t")[7];
 
-	outputArea.value = "Erstellungsdatum: " + creationDate + 
-										"\nSAP Summe: " + hardcodedSum +
-										"\nTest: " + calculateSum();
+  outputArea.value =
+    "Erstellungsdatum: " +
+    creationDate +
+    "\nSAP Summe: " +
+    hardcodedSum +
+    "\nTest: " +
+    calculateSum().toLocaleString("de-DE");
 }
 
 // Eigentlich ueberfluessig.
 function createHeadElements(header) {
-	header.forEach((head, index) => {
-		const input = document.createElement("input");
-		const label = document.createElement("label");
-		const labelId = "label" + index;
-		input.setAttribute("type", "checkbox");
-		label.setAttribute("id", labelId);
-		label.appendChild(input);
-		document.getElementById("checkBox").appendChild(label);
-		const content = document.createTextNode(head);
-		document.getElementById(labelId).appendChild(content);		
-	});
-	
+  header.forEach((head, index) => {
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    const labelId = "label" + index;
+    input.setAttribute("type", "checkbox");
+    label.setAttribute("id", labelId);
+    label.appendChild(input);
+    document.getElementById("checkBox").appendChild(label);
+    const content = document.createTextNode(head);
+    document.getElementById(labelId).appendChild(content);
+  });
 }
 
 function createHeader(rowArray) {
-	const tmpData = rowArray[headerRow].split("\t");
-	tmpData.splice(emptyFourthRow, 1);
-	tmpData.splice(0, emptyFirstRows);
-	return tmpData;
+  const tmpData = rowArray[headerRow].split("\t");
+  tmpData.splice(emptyFourthRow, 1);
+  tmpData.splice(0, emptyFirstRows);
+  return tmpData;
 }
 
 function createData(rowArray, header, emptyFirstRows) {
-	let rowData = [];
-	const betragIndex = findRowByName(betragKopf);
-	for (let i = rmRowsTop; i < rowArray.length - rmRowsBottom; i++) {
-		const tmpData = rowArray[i].split("\t");
-		tmpData.splice(emptyFourthRow, 1);
-		tmpData.splice(0, emptyFirstRows);
-		tmpData[betragIndex] = parseFloat(tmpData[betragIndex].replace(".","").replace(",","."));		
-		rowData.push(tmpData);
-	}
-	return rowData;
+  let rowData = [];
+  const betragIndex = findRowByName(betragKopf);
+  for (let i = rmRowsTop; i < rowArray.length - rmRowsBottom; i++) {
+    const tmpData = rowArray[i].split("\t");
+    tmpData.splice(emptyFourthRow, 1);
+    tmpData.splice(0, emptyFirstRows);
+    tmpData[betragIndex] = parseFloat(
+      tmpData[betragIndex].replace(".", "").replace(",", ".")
+    );
+    rowData.push(tmpData);
+  }
+  return rowData;
 }
 
 function uploadFile(fileInput) {
-	let file = fileInput.files[0];
-	let reader = new FileReader();
-		
-	reader.readAsText(file, "windows-1252");
-	reader.onload = function (e) {
-		const rawData = e.target.result;
-		processData(rawData);
-	};	
+  let file = fileInput.files[0];
+  let reader = new FileReader();
+
+  reader.readAsText(file, "windows-1252");
+  reader.onload = function (e) {
+    const rawData = e.target.result;
+    processData(rawData);
+  };
 }
 
 // Statische Ankerpunkte zum Parsen der Datei
